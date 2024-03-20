763feb6fca0ed3989ae5a539b2f74f9a281f5357
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-03-20 09:19:02 +0300

    completes string lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Wesley Ali';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const welcomeMessage = 'Welcome to Flatbook, ${Wesley Ali}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
